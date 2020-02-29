ee9b017c1124e6ab88c61ef59b6ca2d1f1647a67
ShutongHanDoxa <shutong.han@doxa-holdings.com>
2020-02-29 19:34:02 +0800

    created two components with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import {WarningAlertComponent} from './warningAlert/warningAlert.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/warningAlert/warningAlert.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:"app-warning",
+    template:'<p>Here is a warning.</p>',
+    styles:['p{ color:red}']
+})
+export class WarningAlertComponent{
+
+}
